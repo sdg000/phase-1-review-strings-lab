3b0d168c498282bb777f88804b8fd5ad5e12f103
Seth David Gyimah <esec26@gmail.com>
2022-06-07 12:28:49 +0000

    phase 1- review- strings - lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = "Gracie";
+const welcomeMessage = `Welcome to Flatbook, + ${currentUser} !`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, + ${currentUser[0]} !`;
+
